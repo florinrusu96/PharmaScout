f9eb5023e54968b4eeccadebc2ff1d964fd5c659
andrpangr <andrpangr@gmail.com>
2016-11-19 15:44:06 +0200

    Google Map Key Added

--- PharmaScout/app/src/main/java/pharmascout/devhacks/pharmascout/SearchActivity.java
+++ PharmaScout/app/src/main/java/pharmascout/devhacks/pharmascout/SearchActivity.java
@@ -9,6 +9,8 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-	//comment pus de Nelu
+	    //comment pus de Nelu
+
+
     }
 }
