9c1ecb962aa3075cbeccbf378aafa6097cbe4b87
FlyingZ <rusuflorinandrei96@gmail.com>
2016-11-19 18:55:47 +0200

    6:55/Layout/Option bar/

--- PharmaScout/app/src/main/java/pharmascout/devhacks/pharmascout/MapsActivity.java
+++ PharmaScout/app/src/main/java/pharmascout/devhacks/pharmascout/MapsActivity.java
@@ -1,8 +1,10 @@
 package pharmascout.devhacks.pharmascout;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Nea Florin on 11/19/2016.
  */
 
-public class MapsActivity {
+public class MapsActivity extends AppCompatActivity {
 }
--- PharmaScout/app/src/main/java/pharmascout/devhacks/pharmascout/OptionsScreen.java
+++ PharmaScout/app/src/main/java/pharmascout/devhacks/pharmascout/OptionsScreen.java
@@ -1,8 +1,28 @@
 package pharmascout.devhacks.pharmascout;
 
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
 /**
  * Created by Nea Florin on 11/19/2016.
  */
 
-public class OptionsScreen {
+public class OptionsScreen extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.option_screen);
+        //Option text
+        TextView optionsText = (TextView) findViewById(R.id.rangeText);
+        Typeface face = Typeface.createFromAsset(getAssets(),
+                "fonts/Prime Regular.otf");
+        String range = getString(R.string.range_eng);
+        optionsText.setTypeface(face);
+        optionsText.setTextSize(30);
+        optionsText.setText(range);
+
+    }
 }
