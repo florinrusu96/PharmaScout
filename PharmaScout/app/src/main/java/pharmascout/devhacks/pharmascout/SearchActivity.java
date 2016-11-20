36a293bb7b25ed30dd5d5e4b62ae10971b710a2a
andrpangr <andrpangr@gmail.com>
2016-11-20 06:33:51 +0200

    Almolst there

--- PharmaScout/app/src/main/java/pharmascout/devhacks/pharmascout/SearchActivity.java
+++ PharmaScout/app/src/main/java/pharmascout/devhacks/pharmascout/SearchActivity.java
@@ -1,10 +1,17 @@
 package pharmascout.devhacks.pharmascout;
 
+import android.app.FragmentTransaction;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +24,11 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -44,37 +56,106 @@ public class SearchActivity extends AppCompatActivity {
     private ProgressDialog progressDialog;
     public static final int MAX_DISPLAY_NUMBER = 5;
 
+    double myLatitude;
+    double myLongitude;
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            myLatitude = location.getLatitude();
+            myLongitude = location.getLongitude();
+
+            TextView test = (TextView) findViewById(R.id.textView);
+            test.setText(" " + myLongitude + " " + myLatitude );
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private final LocationListener mmLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(final Location location) {
+            myLatitude = location.getLatitude();
+            myLongitude = location.getLongitude();
+
+            TextView test = (TextView) findViewById(R.id.textView);
+            test.setText(" " + myLongitude + " " + myLatitude );
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    public ArrayList<String> farmaciiArray;
+    public ArrayList<LatLng> coordsArray;
+    public ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
         progressDialog = new ProgressDialog(this);
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+
+            LocationManager mmLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mmLocationListener);
+
+            TextView test = (TextView) findViewById(R.id.textView);
+            test.setText(" " + myLongitude + " " + mLocationManager.toString());
+        } else {
+            // Show rationale and request permission. Joking, sudden nothing here
+        }
+
         //comment pus de Nelu
 
         ListView listView = (ListView) findViewById(R.id.listView );
 
-        ArrayList<String> listItems=new ArrayList<String>();
+        farmaciiArray = new ArrayList<String>();
+        coordsArray = new ArrayList<LatLng>();
 
-        listItems.add("Clicked : "+ 1);
-        listItems.add("Clicked : "+ 12);
-        listItems.add("Clicked : "+ 3);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        adapter = new ArrayAdapter<String>(this,
                                     android.R.layout.simple_list_item_1,
-                                    listItems);
+                                    farmaciiArray);
 
         listView.setAdapter(adapter);
-
-        adapter.add("New Item");
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Bundle coords = new Bundle();
                 Random rand = new Random();
-                coords.putDouble("latitude", 90 * rand.nextDouble());
-                coords.putDouble("longitude", 90 * rand.nextDouble());
+                coords.putDouble("latitude", ((LatLng)coordsArray.get(position)).latitude);
+                coords.putDouble("longitude", ((LatLng)coordsArray.get(position)).longitude);
 
                 Intent intent = new Intent(SearchActivity.this, MapsActivity.class );
                 intent.putExtras(coords);
@@ -137,6 +218,8 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     private void search(String query) {
+        adapter.clear();
+        coordsArray.clear();
         setLoading(true);
         (new RestDBApi()).getFarmacii(query, new RestDBApi.CallBack(){
             @Override
@@ -149,6 +232,17 @@ public class SearchActivity extends AppCompatActivity {
 
                     if (listaFarmacii.size() > MAX_DISPLAY_NUMBER) {
                         handleTooManyResults(listaFarmacii);
+                    }
+                    else{
+                        int index = 0;
+
+                        for( FarmacieModel farmacie : listaFarmacii ){
+                            LatLng coord = new LatLng(farmacie.getLatitudine(), farmacie.getLongitudine());
+
+                            coordsArray.add(index++, coord);
+                            adapter.add(index + " " + farmacie.getNume() + ", " + farmacie.getAdresaFarmacie() + " "
+                                                    + farmacie.getProgram() );
+                        }
                     }
 
                 }
